52e2058ae6749adff0a16c000b0cfc9005ffd356
José Salgado <jjtfsalgado@gmail.com>
2017-01-20 13:20:12 +0000

    Update

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 var express = require('express');
 
-//Create our app
+// Create our app
+const path = require('path');
 var app = express();
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(function (req,res,next){
-  if (req.header['x-forwarded-proto']==='https') {
-    res.redirect('http://' + req.hostname + req.url)
-  }else {
-    next();
+app.use(function (req, res, next) {
+
+  if (req.headers['x-forwarded-proto' === 'https']) {
+      res.redirect('http://' + req.hostname + req.url);
+  } else {
+      next();
   }
 });
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, function () {
-  console.log('Express server is up on port '+ PORT);
-})
+  console.log('Express server is up on port ' + PORT);
+});
