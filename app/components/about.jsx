7e823f62a7a4d5d9866e15ce6a7dacd6466f53db
José Salgado <jjtfsalgado@gmail.com>
2017-01-20 16:24:33 +0000

    Styling 'About' section

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 var About = React.createClass({
   render: function () {
     return(
-      <h3>About Component</h3>
+      <div>
+        <h1 className="text-center"> Examples</h1>
+        <p className="text-left">Vivamus, mea Lesbia, atque amemus,rumoresque senum severiorum omnes unius aestimemus assis!soles occidere et redire possunt: nobis cum semel occidit brevis lux, nox est perpetua una dormienda. da mi basia mille, deinde centum,dein mille altera, dein secunda centum, deinde usque altera mille, deinde centum. dein, cum milia multa fecerimus, conturbabimus illa, ne sciamus, aut ne quis malus invidere possit, cum tantum sciat esse basiorum.</p>
+      </div>
     )
   }
 });
