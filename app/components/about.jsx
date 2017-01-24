14f4b9c3eac3ea82b11e70bd6bc9e1980b755320
José Salgado <jjtfsalgado@gmail.com>
2017-01-24 12:37:32 +0000

    Error modal and style update

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -4,7 +4,7 @@ var About = React.createClass({
   render: function () {
     return(
       <div>
-        <h1 className="text-center"> About </h1>
+        <h1 className="text-center page-title"> About </h1>
         <p className="text-left">Vivamus, mea Lesbia, atque amemus,rumoresque senum severiorum omnes unius aestimemus assis!soles occidere et redire possunt: nobis cum semel occidit brevis lux, nox est perpetua una dormienda. da mi basia mille, deinde centum,dein mille altera, dein secunda centum, deinde usque altera mille, deinde centum. dein, cum milia multa fecerimus, conturbabimus illa, ne sciamus, aut ne quis malus invidere possit, cum tantum sciat esse basiorum.</p>
       </div>
     )
